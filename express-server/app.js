0210d3e4792d1c84b7815671a0a0fe1c1bed333a
nurlan985 <nurlan985@gmail.com>
2020-06-10 21:27:11 -0500

    error handling

--- express-server/app.js
+++ express-server/app.js
@@ -30,14 +30,10 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use(function(err, req, res, next) {
+  let errStatus = err.status || 500;
+  let dev = true; //req.app.get('env') === 'development'
+  res.status(errStatus).json(new ApiResponse(errStatus, 'error', {message:err.message, error:dev ? err : {}}));
 });
 
 mongoose
