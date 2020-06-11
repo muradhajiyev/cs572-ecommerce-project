97d550ccc3326c87d3ec52d828183c1928c94e13
Amin <amin030296@gmail.com>
2020-06-10 19:36:03 -0500

    Approve/Reject User

--- express-server/controllers/admin.js
+++ express-server/controllers/admin.js
@@ -1,14 +1,29 @@
-const Admin = require("../models/admin");
 const User = require("../models/user");
 
+// Handle Approve Seller
 exports.approveSeller = (req, res, next) => {
   const id = req.params.id;
-  res.send(`Approve ${id}`);
-  next();
+
+  User.findById(id)
+    .then((user) => {
+      user.status = "Active";
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(404).send({ message: "user not found" });
+    });
 };
 
+// Handle Reject Seller
 exports.rejectSeller = (req, res, next) => {
   const id = req.params.id;
-  res.send(`Reject ${id}`);
-  next();
+
+  User.findById(id)
+    .then((user) => {
+      user.status = "Reject";
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(404).send({ message: "user not found" });
+    });
 };
