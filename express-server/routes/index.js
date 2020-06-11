e83e7959184a2423d5deedc6a02c9b17623854d7
nurlan985 <nurlan985@gmail.com>
2020-06-10 22:00:18 -0500

    move admin route and rename: admin route, admin controller

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -2,13 +2,14 @@ const
     path = require('path'),
     authRoute = require(path.join(__dirname, 'auth-route')),
     buyerRoute = require(path.join(__dirname, 'buyer-route')),
-    
+    adminRouter = require(path.join(__dirname, "admin-route")),
     { verifyToken, authorizeAdmin, authorizeSeller, authorizeBuyer } = require(path.join(__dirname, 'security'))
     ;
 
 module.exports = function(app){
     app.use('/api/auth', authRoute);
     app.use('/api/buyer', verifyToken, authorizeBuyer, buyerRoute);
+    app.use('/api/admin', verifyToken, authorizeAdmin, adminRouter);
     app.get('/', function(req, res, next) {
         res.send('Online market API is work');
     });
