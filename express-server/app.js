c31fb89119eb05375006210ab7b6b4a83ac8a2bc
joesleiman <joe.c.sleiman@gmail.com>
2020-06-10 20:54:23 -0500

    solve the conflicts and adjust buyer-controller and buyer-route

--- express-server/app.js
+++ express-server/app.js
@@ -34,16 +34,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({error:'error'});
 });
 
-//onlineshoping is the database name
-// mongoose.connect('mongodb://localhost:27017/onlineshopping', {useNewUrlParser: true, useUnifiedTopology: true})
-//     .then(() => {
-//       app.listen(3000, () => {
-//         console.log('Server is running on 3000');
-//       })
-//     })
-//     .catch(err => console.log(err));
 
 module.exports = app;
--- express-server/routes/buyer-route.js
+++ express-server/routes/buyer-route.js
@@ -3,12 +3,11 @@ const
     router = require('express').Router(),
     { buyerController } = require(path.join(__dirname, '..', 'controllers'));
 
-router.get('/addresses', buyerController.allAddresses);
+router.get('/addresses', buyerController.getAllAddresses);
 router.post('/addresses', buyerController.addAddress);
-router.put('/addresses/:id', buyerController.updateAddress);
-router.get('/addresses/:id', buyerController.getAddress);
 router.delete('/addresses/:id', buyerController.deleteAddress);
-
+router.get('/addresses/:id', buyerController.getAddressDetail);
+router.put('/addresses/:id', buyerController.editAddress);
 
 
 module.exports = router;
