f6da61a4070bdae263bca8d317e110ac92a5ebf6
Amin <amin030296@gmail.com>
2020-06-13 18:27:36 -0500

    fix

--- express-server/routes/buyer-route.js
+++ express-server/routes/buyer-route.js
@@ -2,14 +2,14 @@ const path = require("path"),
   router = require("express").Router(),
   { buyerController } = require(path.join(__dirname, "..", "controllers"));
 
-//MARK:- Address cart CURD Operation
+//MARK:- Address cart CRUD Operation
 router.get("/addresses", buyerController.getAllAddresses);
 router.post("/addresses", buyerController.addAddress);
 router.delete("/addresses/:id", buyerController.deleteAddress);
 router.get("/addresses/:id", buyerController.getAddressDetail);
 router.put("/addresses/:id", buyerController.editAddress);
 
-//MARK:- Shopping cart CURD Operation
+//MARK:- Shopping cart CRUD Operation
 router.get("shopping-cart", buyerController.getListShoppingCard);
 router.post("shopping-cart", buyerController.addProductToShoppingCart);
 router.put("shopping-cart/:id", buyerController.updateProductFromShoppingCart);
