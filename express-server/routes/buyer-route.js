89d36ba08437d9839e5628500b59c268d4c6a6df
Amin <amin030296@gmail.com>
2020-06-13 18:26:48 -0500

    CRUD shopping cart

--- express-server/routes/buyer-route.js
+++ express-server/routes/buyer-route.js
@@ -2,14 +2,14 @@ const path = require("path"),
   router = require("express").Router(),
   { buyerController } = require(path.join(__dirname, "..", "controllers"));
 
-//MARK:- Address cart CRUD Operation
+//MARK:- Address cart CURD Operation
 router.get("/addresses", buyerController.getAllAddresses);
 router.post("/addresses", buyerController.addAddress);
 router.delete("/addresses/:id", buyerController.deleteAddress);
 router.get("/addresses/:id", buyerController.getAddressDetail);
 router.put("/addresses/:id", buyerController.editAddress);
 
-//MARK:- Shopping cart CRUD Operation
+//MARK:- Shopping cart CURD Operation
 router.get("shopping-cart", buyerController.getListShoppingCard);
 router.post("shopping-cart", buyerController.addProductToShoppingCart);
 router.put("shopping-cart/:id", buyerController.updateProductFromShoppingCart);
@@ -17,7 +17,6 @@ router.delete(
   "shopping-cart/:id",
   buyerController.deleteProductFromShoppingCart
 );
-
 //MARK:- Billing CRUD
 router.get("/billing-info", buyerController.getAllBillingInfo);
 router.post("/billing-info", buyerController.addBillingInfo);
