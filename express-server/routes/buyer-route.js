013674db88ce38484adec69f9f238ec0b16a0d90
Amin <amin030296@gmail.com>
2020-06-13 18:26:48 -0500

    CRUD shopping cart

--- express-server/routes/buyer-route.js
+++ express-server/routes/buyer-route.js
@@ -1,13 +1,21 @@
-const
-    path = require('path'),
-    router = require('express').Router(),
-    { buyerController } = require(path.join(__dirname, '..', 'controllers'));
+const path = require("path"),
+  router = require("express").Router(),
+  { buyerController } = require(path.join(__dirname, "..", "controllers"));
 
-router.get('/addresses', buyerController.getAllAddresses);
-router.post('/addresses', buyerController.addAddress);
-router.delete('/addresses/:id', buyerController.deleteAddress);
-router.get('/addresses/:id', buyerController.getAddressDetail);
-router.put('/addresses/:id', buyerController.editAddress);
+//MARK:- Address cart CURD Operation
+router.get("/addresses", buyerController.getAllAddresses);
+router.post("/addresses", buyerController.addAddress);
+router.delete("/addresses/:id", buyerController.deleteAddress);
+router.get("/addresses/:id", buyerController.getAddressDetail);
+router.put("/addresses/:id", buyerController.editAddress);
 
+//MARK:- Shopping cart CURD Operation
+router.get("shopping-cart", buyerController.getListShoppingCard);
+router.post("shopping-cart", buyerController.addProductToShoppingCart);
+router.put("shopping-cart/:id", buyerController.updateProductFromShoppingCart);
+router.delete(
+  "shopping-cart/:id",
+  buyerController.deleteProductFromShoppingCart
+);
 
 module.exports = router;
