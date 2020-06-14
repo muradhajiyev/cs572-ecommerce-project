2666063c57a09012038222dabc9144854e0e340c
joesleiman <joe.c.sleiman@gmail.com>
2020-06-13 22:29:55 -0500

    Crud billing info, create, delete, get active reviews follow and unfollow seller

--- express-server/models/index.js
+++ express-server/models/index.js
@@ -2,11 +2,15 @@ const path = require('path'),
     ApiResponse = require(path.join(__dirname, 'ApiResponse')),
     User = require(path.join(__dirname, 'user')),
     Seller = require(path.join(__dirname, 'seller')),
-    Buyer = require(path.join(__dirname, 'buyer'));
+    Buyer = require(path.join(__dirname, 'buyer')),
+    Product = require(path.join(__dirname, 'product')),
+    Order = require(path.join(__dirname, 'order'));
 
 module.exports = {
     ApiResponse,
     User,
     Seller,
-    Buyer
+    Buyer,
+    Product,
+    Order
 }
