28f1338870837622d90584005bdb84b05b90a2be
joesleiman <joe.c.sleiman@gmail.com>
2020-06-14 23:22:59 -0500

    refactor follow user

--- express-server/routes/user.route.js
+++ express-server/routes/user.route.js
@@ -3,7 +3,6 @@ const { userController } = require("../controllers");
 const Role = require('../_helpers/role');
 const authorize = require('../_helpers/authorize');
 
-// todo: change follow Seller implementation.
 router.post("/sellers/:id/follow", authorize(Role.BUYER),  userController.followSeller);
 router.delete("/sellers/:id/unfollow/", authorize(Role.BUYER), userController.unfollowSeller);
 
