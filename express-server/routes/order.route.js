987337cfcd86d8f9f36716357921b83f35649ab0
Murad Haciyev <murad.hajiyev@certoclav.com>
2020-06-15 15:38:14 -0500

    generates receipts, service part should be implemented

--- express-server/routes/order.route.js
+++ express-server/routes/order.route.js
@@ -7,4 +7,6 @@ router.post("/", authorize(Role.BUYER), orderController.createOrder);
 router.put("/:orderId/cancel", authorize(Role.BUYER, Role.SELLER), orderController.cancelOrder);
 router.get("/", authorize(Role.BUYER), orderController.getMyOrders);
 
+router.post("/receipt", orderController.generateReceipt);
+
 module.exports = router;
