47e570e9e54c6a07553d2ae80da2bbaaf02004d2
Murad Haciyev <murad.hajiyev@certoclav.com>
2020-06-16 13:51:59 -0500

    completes AuthService and adds jwtinterceptor.

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -13,10 +16,13 @@ import { HomeComponent } from './components/home/home.component';
     HomeComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-client/src/app/modules/authentication/login/login.component.ts
+++ angular-client/src/app/modules/authentication/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   templateUrl: './login.component.html',
@@ -7,9 +8,22 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.login();
+  }
+
+
+  login() {
+    this.authService.login("corozza@gmail.com","123").subscribe(
+      res => {
+        console.log('success', res);
+      },
+      error => {
+        console.log('error', error);
+    }
+    )
   }
 
 }
