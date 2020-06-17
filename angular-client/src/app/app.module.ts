b63e212f53b06d13e156f8ca1af4e57219595349
Amin <amin030296@gmail.com>
2020-06-17 12:43:28 -0500

    form view is done

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateProductForm } from './components/create-product-form/create-product-form.component';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +22,7 @@ import { CreateProductForm } from './components/create-product-form/create-produ
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
