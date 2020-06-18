95b71d372aec44c105e1b63bec21b25dabad6f98
Murad Haciyev <hajiyev.murad1@gmail.com>
2020-06-18 01:15:54 -0500

    adds redirect guard for profile

--- angular-client/src/app/modules/admin/admin-routing.module.ts
+++ angular-client/src/app/modules/admin/admin-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminComponent} from "../../components/admin/admin.component";
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { Role } from 'src/app/models';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'view' },
-  { path: 'view', component: AdminComponent},
+  { path: 'view', component: AdminComponent, canActivate: [AuthGuard], data: {roles: [Role.Admin]}},
 ];
 
 @NgModule({
