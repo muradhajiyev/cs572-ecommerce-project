feaa96120243eadc9110a7483a85922eee43af25
Amin <amin030296@gmail.com>
2020-06-17 21:03:48 -0500

    add product almost done

--- angular-client/src/app/components/create-product-form/create-product-form.component.ts
+++ angular-client/src/app/components/create-product-form/create-product-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/app/models';
+import { ProductForm } from 'src/app/models/product-form';
 
 @Component({
   selector: 'create-product-form',
@@ -9,11 +11,19 @@ import { CategoryService } from 'src/app/services/category.service';
   styleUrls: ['./create-product-form.component.css'],
 })
 export class CreateProductForm implements OnInit {
+  productForm = new ProductForm();
+  category: Array<Category>;
+  debug = 'Empty';
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((categories) => {
-      console.log('CATEGORY : ' + categories);
+      this.category = categories.result;
     });
+  }
+
+  onSubmitForm(f) {
+    this.debug = `${this.productForm}`;
+    console.log('Form Values ' + f);
   }
 }
---
+++ angular-client/src/app/models/product-form.ts
@@ -0,0 +1,7 @@
+export class ProductForm {
+  public title: String;
+  public categoryId: string;
+  public price: string;
+  public filename: string;
+  public description: string;
+}
