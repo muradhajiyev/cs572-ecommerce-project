011fa3e490f8fd0596f44f76870208136a444c1b
Murad Haciyev <hajiyev.murad1@gmail.com>
2020-06-18 13:28:10 -0500

    implements signup and fix email validaiton in serverside.

--- angular-client/src/app/modules/authentication/authentication-routing.module.ts
+++ angular-client/src/app/modules/authentication/authentication-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
-  // { path: 'register', component: RegisterComponent },
+  { path: 'signup', component: SignupComponent },
 ]
 
 @NgModule({
