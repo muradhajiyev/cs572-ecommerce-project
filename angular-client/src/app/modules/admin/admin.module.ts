989ed298da63ca383e9b3e7a848264508e1dd798
Murad Haciyev <hajiyev.murad1@gmail.com>
2020-06-17 20:25:12 -0500

    fixes component issue

--- angular-client/src/app/modules/admin/admin.module.ts
+++ angular-client/src/app/modules/admin/admin.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import {AdminRoutingModule} from "./admin-routing.module";
 import {AdminComponent} from "../../components/admin/admin.component";
 import {SharedModule} from "../shared/shared.module";
-import {AdminPendingTasksComponent} from "../../components/admin-pending-tasks/admin-pending-tasks.component";
+// import {AdminPendingTasksComponent} from "../../components/admin-pending-tasks/admin-pending-tasks.component";
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    AdminPendingTasksComponent
+    // AdminPendingTasksComponent
   ],
   imports: [
     CommonModule,
